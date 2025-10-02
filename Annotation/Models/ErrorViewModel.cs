85b2d50d362ffa9d1656f5bf6c2836634a0e6bc2
siphuc05 <nguyensiphuc26052005@gmail.com>
2025-10-02 22:03:20 +0700

    first commit

--- Annotation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Annotation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
